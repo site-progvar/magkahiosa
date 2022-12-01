ee107f06b3320548b99b1fcdcee501d34f246b21
MarkRywell <gopro855@gmail.com>
2022-12-01 15:13:07 +0800

    Added brand-icon

---
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -0,0 +1,4 @@
+<div class="flex gap-3 items-center justify-center h-10 w-full">
+    <img class="h-10 object-contain" src="{{ asset('image/MagkahiOSA Logo.svg') }}" alt="">
+    <img class="h-full w-24 object-contain" src="{{ asset('image/MagkahiOSA Logo (2).png') }}" alt="">
+</div>
